f603268b4bda4cc147c8f69cf85f20620f8e4ac9
Student E <student-e@users.noreply.github.com>
2021-05-05 16:28:13 +0200

    DescriptivePlotsV2

--- wrapper.R
+++ wrapper.R
@@ -12,19 +12,35 @@ source("helpers/Functions.R")
 # Input parameters for the automatic .Rmd file 
 # Set values 
 ##############################################################
-filename <- "recovery.csv"
+filename <- "mtept.csv"
 path = paste(file.path("Datasets"), sep="/", filename)
 decimal <- "."
 data <- fread(path, header = "auto", sep ="auto", dec = decimal, 
               encoding ="UTF-8", data.table = FALSE, na.strings = "")
 ## Selection of variables
-vars1 <- c("blanket", "minutes") 
+vars1 <- c("V1", "treatment","E1","E2","E3","E4") 
 ## Model Information
-model <- "minutes ~ blanket \n blanket \n b0"
+model <- "V1 ~ treatment+E1+E2+E3+E4 \n treatment \n Drug"
 ## Criterium to identify continuous vs discrete variables 
 cont_crit <- "Liberal" # or Conservative 
 
 
+params = list(
+  filename = filename,
+  decimal = decimal,
+  data = data,
+  vars1 = vars1,
+  model = model,
+  cont_crit = cont_crit 
+)
+
+msyntax <- TRUE
+model <- params$model
+modelsplit <- unlist(strsplit(model, split="\n"))
+modelsplit <- gsub(" ", "", modelsplit, fixed = TRUE)
+modelsplit <- modelsplit[modelsplit != ""]
+
+rm(params)
 
 ##############################################################
 # Run parametric, automatic .Rmd file 

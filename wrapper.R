ce0cf6832ed9ef2fff454460d7f71ef7e4facaf7
Student E <student-e@users.noreply.github.com>
2021-05-19 17:28:08 +0200

    desc_plots_titles

--- wrapper.R
+++ wrapper.R
@@ -12,35 +12,35 @@ source("helpers/Functions.R")
 # Input parameters for the automatic .Rmd file 
 # Set values 
 ##############################################################
-filename <- "mtept.csv"
+filename <- "immer.csv"
 path = paste(file.path("Datasets"), sep="/", filename)
 decimal <- "."
 data <- fread(path, header = "auto", sep ="auto", dec = decimal, 
               encoding ="UTF-8", data.table = FALSE, na.strings = "")
 ## Selection of variables
-vars1 <- c("treatment","E1", "E2", "E3","E4")
+vars1 <- c("Loc","Var", "Y1", "Y2")
 ## Model Information
-model <- "E1 ~ treatment + E2 + E3 \n treatment \n Placebo"
+model <- "Y1 ~ Loc + Var + Y2 \n Loc \n UF"
 ## Criterium to identify continuous vs discrete variables 
 cont_crit <- "Liberal" # or Conservative Liberal 
 
 
-# params = list(
-#   filename = filename,
-#   decimal = decimal,
-#   data = data,
-#   vars1 = vars1,
-#   model = model,
-#   cont_crit = cont_crit
-# )
-# 
-# msyntax <- TRUE
-# model <- params$model
-# modelsplit <- unlist(strsplit(model, split="\n"))
-# modelsplit <- gsub(" ", "", modelsplit, fixed = TRUE)
-# modelsplit <- modelsplit[modelsplit != ""]
-# 
-# rm(params)
+params = list(
+  filename = filename,
+  decimal = decimal,
+  data = data,
+  vars1 = vars1,
+  model = model,
+  cont_crit = cont_crit
+)
+
+msyntax <- TRUE
+model <- params$model
+modelsplit <- unlist(strsplit(model, split="\n"))
+modelsplit <- gsub(" ", "", modelsplit, fixed = TRUE)
+modelsplit <- modelsplit[modelsplit != ""]
+
+rm(params)
 
 ##############################################################
 # Run parametric, automatic .Rmd file 

3d675a22e49aa4f8711e7031cf6c800b75f7faab
vdaum <vdaum@hs-koblenz.de>
2021-06-03 14:04:06 +0200

    Add reports

--- wrapper.R
+++ wrapper.R
@@ -12,19 +12,19 @@ source("helpers/Functions.R")
 # Input parameters for the automatic .Rmd file 
 # Set values 
 ##############################################################
-filename <- "immer.csv"
+filename <- "recovery.csv"
 path = paste(file.path("Datasets"), sep="/", filename)
 decimal <- "."
 data <- fread(path, header = "auto", sep ="auto", dec = decimal, 
               encoding ="UTF-8", data.table = FALSE, na.strings = "")
 ## Selection of variables
-vars1 <- c("Loc","Var", "Y1", "Y2")
+vars1 <- c("blanket", "minutes") 
 ## Model Information
-model <- "Y1 ~ Loc + Var + Y2 \n Loc \n UF"
+model <- "minutes ~ blanket \n blanket \n b0"
 ## Criterium to identify continuous vs discrete variables 
 cont_crit <- "Liberal" # or Conservative Liberal 
 
-
+# 
 # params = list(
 #   filename = filename,
 #   decimal = decimal,

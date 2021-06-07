6c79e3ce7c8439adb6c430afd42675b69ff52c8f
vdaum <vdaum@hs-koblenz.de>
2021-06-07 12:20:06 +0200

    Add interaction plot

--- wrapper.R
+++ wrapper.R
@@ -12,35 +12,35 @@ source("helpers/Functions.R")
 # Input parameters for the automatic .Rmd file 
 # Set values 
 ##############################################################
-filename <- "recovery.csv"
+# Dataset warpbreaks, two-factorial 
+filename <- "warpbreaks.csv"
 path = paste(file.path("Datasets"), sep="/", filename)
 decimal <- "."
 data <- fread(path, header = "auto", sep ="auto", dec = decimal, 
               encoding ="UTF-8", data.table = FALSE, na.strings = "")
 ## Selection of variables
-vars1 <- c("blanket", "minutes") 
+vars1 <- c("breaks", "wool", "tension") 
 ## Model Information
-model <- "minutes ~ blanket \n blanket \n b0"
-## Criterium to identify continuous vs discrete variables 
+model <- "breaks ~ wool+tension \n wool \n A"
 cont_crit <- "Liberal" # or Conservative Liberal 
 
 
-params = list(
-  filename = filename,
-  decimal = decimal,
-  data = data,
-  vars1 = vars1,
-  model = model,
-  cont_crit = cont_crit
-)
-
-msyntax <- TRUE
-model <- params$model
-modelsplit <- unlist(strsplit(model, split="\n"))
-modelsplit <- gsub(" ", "", modelsplit, fixed = TRUE)
-modelsplit <- modelsplit[modelsplit != ""]
-
-rm(params)
+# params = list(
+#   filename = filename,
+#   decimal = decimal,
+#   data = data,
+#   vars1 = vars1,
+#   model = model,
+#   cont_crit = cont_crit
+# )
+# 
+# msyntax <- TRUE
+# model <- params$model
+# modelsplit <- unlist(strsplit(model, split="\n"))
+# modelsplit <- gsub(" ", "", modelsplit, fixed = TRUE)
+# modelsplit <- modelsplit[modelsplit != ""]
+# 
+# rm(params)
 
 ##############################################################
 # Run parametric, automatic .Rmd file 

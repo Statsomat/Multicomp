fcadaf2c21cafe69b404edcf7ec17b85b59890f9
vdaum <vdaum@hs-koblenz.de>
2021-06-10 10:29:50 +0200

    Update wrapper.R

--- wrapper.R
+++ wrapper.R
@@ -2,7 +2,6 @@
 library(rmarkdown)
 library(data.table)
 library(shiny)
-library(tinytex)
 
 
 # Import functions 
@@ -12,35 +11,21 @@ source("helpers/Functions.R")
 # Input parameters for the automatic .Rmd file 
 # Set values 
 ##############################################################
-# Dataset warpbreaks, two-factorial 
-filename <- "warpbreaks.csv"
+filename <- "litter.csv"
 path = paste(file.path("Datasets"), sep="/", filename)
 decimal <- "."
 data <- fread(path, header = "auto", sep ="auto", dec = decimal, 
               encoding ="UTF-8", data.table = FALSE, na.strings = "")
 ## Selection of variables
-vars1 <- c("breaks", "wool", "tension") 
+vars1 <- c("weight", "dose", "gesttime", "number") 
 ## Model Information
-model <- "breaks ~ wool+tension \n wool \n A"
-cont_crit <- "Liberal" # or Conservative Liberal 
+model <- "weight ~ dose + gesttime + number \n dose \n 0"
 
 
-# params = list(
-#   filename = filename,
-#   decimal = decimal,
-#   data = data,
-#   vars1 = vars1,
-#   model = model,
-#   cont_crit = cont_crit
-# )
-# 
-# msyntax <- TRUE
-# model <- params$model
-# modelsplit <- unlist(strsplit(model, split="\n"))
-# modelsplit <- gsub(" ", "", modelsplit, fixed = TRUE)
-# modelsplit <- modelsplit[modelsplit != ""]
-# 
-# rm(params)
+## Criterium to identify continuous vs discrete variables 
+cont_crit <- "Liberal" # or Conservative 
+
+
 
 ##############################################################
 # Run parametric, automatic .Rmd file 
@@ -55,8 +40,3 @@ rmarkdown::render("report.Rmd", params = list(
 ))
 
 ############################################## end of program ########################################################################
-
-
-
-
-

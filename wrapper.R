6a6eec2a66808c4e557e36a6d2ec63299319a245
Denise Welsch <denise.welsch@reyar.de>
2021-08-05 16:31:48 +0200

    Update wrapper.R

--- wrapper.R
+++ wrapper.R
@@ -1,44 +1,7 @@
-# Import libraries
-library(rmarkdown)
-library(data.table)
-library(shiny)
-
-
-# Import functions 
-source("helpers/Functions.R")
-
-##############################################################
-# Input parameters for the automatic .Rmd file 
-# Set values 
-##############################################################
-filename <- "warpbreaks.csv"
-path = paste(file.path("Datasets"), sep="/", filename)
-decimal <- "."
-data <- fread(path, header = "auto", sep ="auto", dec = decimal, 
-              encoding ="UTF-8", data.table = FALSE, na.strings = "")
-## Selection of variables
-vars1 <- c("breaks", "wool", "tension") 
-## Model Information
-model <- "breaks ~ wool*tension \n wool \n A \n less"
-##############################################################
+# Attention!
+# The Wrapper does not work anymore! 
+# Please use only the GUI to upload and edit your model: 
+# Open either the server.R file or the ui.R file and click Run app in right upper corner! 
 
 
 
-## Criterium to identify continuous vs discrete variables 
-cont_crit <- "Liberal" # or Conservative 
-
-
-
-##############################################################
-# Run parametric, automatic .Rmd file 
-##############################################################
-rmarkdown::render("report.Rmd", params = list(
-  filename = filename,
-  decimal = decimal,
-  data = data,
-  vars1 = vars1,
-  model = model,
-  cont_crit = cont_crit 
-))
-
-############################################## end of program ########################################################################
